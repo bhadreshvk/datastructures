36a4dfb8b45ecf2a468076ea55e2f31706e210e9
teddybhadru <bhadreshvk@users.noreply.github.com>
2022-08-04 20:26:33 +0530

    modified mergesort

--- mergesort.c
+++ mergesort.c
@@ -1,47 +1,76 @@
 #include<stdio.h>
-int main(){
-    int a[5]={1,9,45,6,3,8};
-    int low=0;int high=5;
-    partition(a,low,high);
+void mergesort(int a[], int l, int r)
 
+{
+if (l < r)
+{
+
+int mid = l+(r-l)/2;
+
+mergesort(a, l, mid);
+mergesort(a, mid+1, r);
+ 
+
+merge(a, l, mid, r);
 }
-void partition(int a[5],int low,int high){
-    if(low<high){
-    int mid=(low+high)/2;
-    partition(a,low,mid);
-    partition(a,mid+1,high);
-    merge(a,low,mid,high);
-}}
-
-void merge(int a[5],int low,int mid,int high){
-    int h,i,j,b[5],k;
-    h=i=low;
-    j=mid+1;
-    while((h<=mid)&&(j<=high)){
-        if(a[h]<=a[j]){
-            b[h]=a[h];
-            h++;
-        }
-        else{
-            b[h]=a[j];
-            j++;
-        }
-        i++;
-    
-
-    while(j<=high){
-        b[i]=a[j];
-        i++;
-        j++;
-    }
-    while(h<=mid){
-        b[i]=a[h];
-        i++;
-        h++;
-    }
 }
-    
-    for(int o=0;o<=5;o++){
-        printf("%d ",b[o]);
-    }
+
+void merge(int a[], int l, int mid, int r)
+{
+int i, j, k;
+int n1 = mid - l + 1;
+int n2 = r - mid;
+
+int L[n1], R[n2];
+
+for (i = 0; i < n1; i++)
+L[i] = a[l + i];
+for (j = 0; j < n2; j++)
+R[j] = a[mid + 1+ j];
+
+i = 0;
+j = 0;
+k = l;
+while (i < n1 && j < n2)
+{
+if (L[i] <= R[j])
+{
+a[k] = L[i];
+i++;
+}
+else
+{
+a[k] = R[j];
+j++;
+}
+k++;
+}
+
+while (i < n1)
+{
+a[k] = L[i];
+i++;
+k++;
+}
+
+while (j < n2)
+{
+a[k] = R[j];
+j++;
+k++;
+}
+}
+int main()
+{
+int a[] = {5,10,6,4,9,13};
+int a_size = sizeof(a)/sizeof(a[0]);
+printf("Given array is \n");
+for(int i=0;i<a_size;i++)
+printf("%d ",a[i]);
+printf("\n");
+mergesort(a, 0, a_size - 1);
+printf("\nSorted array is \n");
+for(int i=0;i<a_size;i++)
+printf("%d ",a[i]);
+return 0;
 }
